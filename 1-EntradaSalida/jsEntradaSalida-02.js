1b1030ef55fe200239a4c77ac9acefa5971808f8
cddavalos <davaloscris@gmail.com>
2020-07-13 01:47:34 -0300

    entrada/salida 1 y 2

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,18 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombre = prompt ('Ingresa tu nombre');
+
+	if(nombre != null && nombre != '') {
+		alert ( nombre );
+	}
+
 }
+
+// 1 guardar texto del prompt.
+// 2 A caso ok, mostrar mensaje con lo que ingreso.
+//	2 A1 ingresa texto, mostrarlo
+//	2 A2 no ingresa texto, no mostrarlo
+// 2 B cancelar, no mostrar mensaje.
+// 
 

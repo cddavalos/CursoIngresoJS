a6f82eb2c6921fe6c3f2810321abfd5a6535301e
cddavalos <davaloscris@gmail.com>
2020-07-13 17:10:16 -0300

    resuelto ej 04

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -2,8 +2,12 @@
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
-function mostrar()
-{
-	alert("ok");
+var texto = prompt ('ingresar texto');
+
+function mostrar() {
+	
+	txtIdNombre.value = texto;
 }
 
+// tomar valor por prompt
+// mostrar valor por ID.value
